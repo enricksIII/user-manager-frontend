7506adaa8dc70a564426f835c2eeae36f6df5082
sajicode <sajioloye@gmail.com>
2018-12-18 03:52:01 +0100

    create file for json-server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+// const { exec } = require('child_process');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3100;
@@ -9,6 +10,17 @@ app.use(express.static(publicPath));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+// const command = `json-server --watch ../db.json --p ${port} -d 2000`;
+
+// exec(command, (err, stdout, stderr) => {
+// 	if (err) {
+// 		console.log('Error running exec', err);
+// 		return;
+// 	}
+// 	console.log('stdout:', stdout);
+// 	console.log('stderr:', stderr);
+// });
 
 app.listen(port, () => {
 	console.log('Server running on port 3100');
